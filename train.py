5f00649839a5741c1510e003620f0c2f98b21bcb
jkalsi1 <jkalsi@calpoly.edu>
2024-10-25 09:55:34 -0700

    started training setup

---
+++ train.py
@@ -0,0 +1,31 @@
+# import tensorflow as tf
+from hands import process
+import os
+import imageio.v3 as imageio
+from matplotlib import pyplot as plt
+import cv2
+
+asl_dir = "asl_dataset"
+sub_dirs = sorted([d for d in os.listdir(asl_dir) if os.path.isdir(os.path.join(asl_dir, d))])
+
+dir_0 = os.path.join(asl_dir,sub_dirs[0])
+for img in os.listdir(dir_0):
+    print(dir_0, img)
+    image_path = os.path.join(dir_0, img)
+    im = imageio.imread(image_path)
+    frame, landmarks = process(im)
+    plt.imshow( frame)
+    print(landmarks)
+
+# # Iterate through each subdirectory in alphabetical order
+# for sub_dir in sub_dirs:
+#     sub_dir_path = os.path.join(asl_dir, sub_dir)
+
+#     images = sorted([img for img in os.listdir(sub_dir_path) if img.endswith(('.jpg', '.jpeg', '.png'))])
+    
+#     # Call the function on each image
+#     for img in images:
+#         image_path = os.path.join(sub_dir_path, img)
+#         im = imageio.imread(image_path)
+#         frame, arr = process(im)
+#         print(arr)
