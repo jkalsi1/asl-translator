8e4b9612d6cd1d447e653e0f43e173c0b4807eda
Meyli Jaeger <mjaege02@calpoly.edu>
2024-11-14 10:27:43 -0800

    quick change

--- webcam-predict.py
+++ webcam-predict.py
@@ -12,7 +12,7 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-hands = mp_hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=0.3)
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 labels_dict = {0: 'A', 1: 'B', 2: 'C'}
 
 cap = cv2.VideoCapture(0)
